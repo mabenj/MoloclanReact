d23fc17ffc2331e17a35c696191504d5689c57f0
mabenj <markobenj@hotmail.com>
2021-07-10 00:03:07 +0300

    WIP remove watermarks of ts viewer

--- src/components/TSViewer/TSViewer.js
+++ src/components/TSViewer/TSViewer.js
@@ -10,6 +10,14 @@ export default function TSViewer() {
 			const ts3v_url_1 = TSViewerUrls.viewerUrl;
 			// eslint-disable-next-line no-undef
 			ts3v_display.init(ts3v_url_1, 1121036, 100);
+			const tsvWatermark = document.querySelector(
+				'[title="TSViewer for TeamSpeak 3 by TSViewer.com"]'
+			);
+			const googlePlayAd = document.querySelector(
+				'[title="TSViewer for Android"]'
+			);
+			tsvWatermark?.parentNode.removeChild(tsvWatermark);
+			googlePlayAd?.parentNode.removeChild(googlePlayAd);
 		};
 		document.body.appendChild(script);
 	}, []);
