9c56e120093d9a1680afef87f9df8117545cf620
mabenj <markobenj@hotmail.com>
2021-07-26 22:00:03 +0300

    Convert ServerIPInfo.js to ServerIPInfo.tsx

--- src/components/ServerPage/ServerIPInfo.tsx
+++ src/components/ServerPage/ServerIPInfo.tsx
@@ -1,14 +1,22 @@
 import { CopyButton, OpenButton } from "../Buttons";
 import { trimProtocolAndQueryString } from "../../Utils";
 
-export default function ServerIPInfo({ urlArray, isLink, linkVerb, header }) {
-	if (urlArray?.length < 1) {
-		return null;
-	}
+interface IServerIPInfo {
+	urlArray: string[];
+	isLink: boolean;
+	linkVerb: string;
+	header?: string;
+}
 
+const ServerIPInfo: React.FC<IServerIPInfo> = ({
+	urlArray,
+	isLink,
+	linkVerb,
+	header = "IP-Osoite"
+}) => {
 	return (
 		<>
-			<h5 className="text-uppercase">{header ? header : "IP-Osoite"}</h5>
+			<h5 className="text-uppercase">{header}</h5>
 			{urlArray.map((url) => (
 				<div key={url} className="server-info">
 					{isLink ? (
@@ -30,4 +38,6 @@ export default function ServerIPInfo({ urlArray, isLink, linkVerb, header }) {
 			))}
 		</>
 	);
-}
+};
+
+export default ServerIPInfo;
