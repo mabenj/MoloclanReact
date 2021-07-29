9496cb44ccca39a427b039a9c4d78289f876fc93
mabenj <markobenj@hotmail.com>
2021-07-29 20:47:35 +0300

    Fix broken waitForElem Util.ts function

--- src/Utils.ts
+++ src/Utils.ts
@@ -37,22 +37,23 @@ export function getYoutubeThumbnailUrl(id: string) {
 }
 
 // https://stackoverflow.com/a/61511955
-export async function waitForElem(
-	selector: string
-): Promise<Element | undefined> {
-	const element = document.querySelector(selector);
-	if (element) {
-		return element;
-	}
-	const observer = new MutationObserver((_) => {
+export function waitForElem(selector: string): Promise<Element | null> {
+	return new Promise<Element | null>((resolve) => {
 		if (document.querySelector(selector)) {
-			observer.disconnect();
-			return document.querySelector(selector);
+			resolve(document.querySelector(selector));
 		}
-	});
-	observer.observe(document.body, {
-		childList: true,
-		subtree: true
+
+		const observer = new MutationObserver(() => {
+			if (document.querySelector(selector)) {
+				resolve(document.querySelector(selector));
+				observer.disconnect();
+			}
+		});
+
+		observer.observe(document.body, {
+			childList: true,
+			subtree: true
+		});
 	});
 }
 
